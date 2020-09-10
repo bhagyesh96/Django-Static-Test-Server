f065c35a5f52f2e47b94b83856bd2b4da61a1c52
Bhagyesh Panchal <bhagyeshpanchal1996@gmail.com>
2020-09-10 15:10:19 +0530

    test

--- notify/static/js/makerobos-sw.js
+++ notify/static/js/makerobos-sw.js
@@ -1,3 +1,3 @@
-importScripts('http://127.0.0.1:5000/static/js/cdn/sw-data.js');
+//importScripts('http://127.0.0.1:5000/static/js/cdn/sw-data.js');
 //importScripts('https://makerobosassets.s3.ap-south-1.amazonaws.com/webpush/sw-data.js');
-//importScripts('http://tifizza.com/cdn/js/sw-data.js');
+importScripts('http://tifizza.com/cdn/js/sw-data.js');
